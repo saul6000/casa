29366ee02b9149464e65491805b3a3f1f8f57e80
LordS <LordS@DESKTOP-VB93U4C>
2020-05-25 09:35:03 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = 10;
+            int b = 20;
+            Console.WriteLine("La multiplicacion de {0} x {1} es {2}",a,b,(a * b));
+            Console.WriteLine("Pulse una tecla para continuar");
+            Console.ReadKey();
+    }
+}
